d20dcd4f9b0d3ecf888ac914ca7ca7d8f9c1e4f6
何旭 <1715924014>
2021-11-10 09:54:33 +0800

    测试分支添加

---
+++ src/main/java/gitLearn.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class gitLearn {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList<String>();
+        list.add("aa");
+        list.add("bb");
+        System.out.println(list.get(1));
+    }
+}
